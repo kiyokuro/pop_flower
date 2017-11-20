1906c2fc3715945a8c782ea39836e61137dc2da6
kiyokuro <kuro6412@gmail.com>
2017-11-20 17:41:30 +0900

    add: routes for event and flower

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :events, only: [:index, :show, :new, :create] do
+    resource :flowers, only: [:new, :create]
+  end
 end
