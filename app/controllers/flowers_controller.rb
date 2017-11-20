2ef971ac7e0af0579e1ebf5321f2467ec0ace738
naoki-k <you@naoki.k.0820.i@gmail.com>
2017-11-20 18:56:27 +0900

    add form for flower

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,4 +1,18 @@
 class FlowersController < ApplicationController
   def new
+    event = Event.find(params[:id])
+    @flower = Flower.new
   end
+
+  def create
+    @flower = event.flower.new(params[:flower])
+    if @flower.save
+      redirect_to events_path
+      # notice[:success]
+    else
+      render 'new'
+    end
+  end
+
+
 end
