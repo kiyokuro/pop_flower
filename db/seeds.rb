0d14ab176cd5365fa111b6bd7849e844c0acb1d8
komoriyuichiro <thekomori1113@gmail.com>
2017-11-20 17:58:55 +0900

    [add] seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# event
+Event.create(title: "誕生日")
+Event.create(title: "母の日")
+Event.create(title: "父の日")
+Event.create(title: "クリスマス")
+Event.create(title: "結婚記念日")
+
+events = Event.all[0..4]
+
+flowers = ["チューリップ", "バラ", "ひまわり"]
+events.each do |event|
+  3.times do |n|
+    Flower.create(name: flowers[n],
+                  event_id: event.id,
+                  votes_num: n)
+  end
+end
