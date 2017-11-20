c9332ffbf2691369ba6b07fe33f1c25fc6f95e36
hotuta <hotuta@users.noreply.github.com>
2017-11-20 17:10:18 +0900

    [fix]コントローラ名を間違えてた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'name/new'
-
-  get 'event/show'
-
-  get 'event/index'
+  root 'events#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
